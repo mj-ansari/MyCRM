3d399f26a6cb6088c3d8ca90cb277879b6ed8b25
mj-ansari <140312886+mj-ansari@users.noreply.github.com>
2023-07-25 17:16:10 +0530

    Update settings.py

--- MyCRM/settings.py
+++ MyCRM/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rcs3lgxb_4lsmpz13=6$42+_m&(py)k)$p^*al$nn&a5c!-h9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Message Alerts
 
